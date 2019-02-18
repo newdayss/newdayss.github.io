f2ac9b860668e0b10970639bf7ebaf85eaa16244
2243393193@qq.com <2243393193@qq.com>
2019-02-18 12:22:54 +0800

    **

---
+++ six/build/precache-manifest.08be84c475ed6d02b9311d39fd628c3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "877d05a5a82872d99bb1",
+    "url": "https://newdayss.github.io/six/build/static/js/runtime~main.877d05a5.js"
+  },
+  {
+    "revision": "ee549a9b132829743ebe",
+    "url": "https://newdayss.github.io/six/build/static/js/main.ee549a9b.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "https://newdayss.github.io/six/build/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "ee549a9b132829743ebe",
+    "url": "https://newdayss.github.io/six/build/static/css/main.6e31bc01.chunk.css"
+  },
+  {
+    "revision": "f5814d20e9373fe4cd0e89769721b2b8",
+    "url": "https://newdayss.github.io/six/build/index.html"
+  }
+];
