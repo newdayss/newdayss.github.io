c1bb4cb6dfd87faecbf8a3cc725eddf17a9ab05c
2243393193@qq.com <2243393193@qq.com>
2019-02-19 16:53:23 +0800

    four

--- four/js/detail.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	var hash_img = document.getElementById('hash_img');
-	var hash = location.hash;
-	
-	if(hash!=''){
-		var newhash = hash.split('#')[1];
-		hash_img.src = newhash;
-	}
-}
